e72c8665efb55396b593776a8fd876641f3cdc40
KJH <daeding7@naver.com>
2022-08-04 12:38:57 +0900

    피자 가게 아이콘 및 이름 연결 완료 -> 세부사항 디테일 화면 작성 X(작성 요망)

--- app/src/main/java/com/kim/pizzaorderapp_kim_practice2/adapters/PizzaStoreListAdapter.kt
+++ app/src/main/java/com/kim/pizzaorderapp_kim_practice2/adapters/PizzaStoreListAdapter.kt
@@ -5,6 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
 import com.kim.pizzaorderapp_kim_practice2.R
 import com.kim.pizzaorderapp_kim_practice2.datas.Store
 
@@ -26,6 +29,15 @@ class PizzaStoreListAdapter(
         }
 
         val row = tempRow!!
+
+        val data = mList[position]
+
+        val pizzaStoreImg = row.findViewById<ImageView>(R.id.pizzaStoreImg)
+        val pizzaStoreNameTxt = row.findViewById<TextView>(R.id.pizzaStoreNameTxt)
+
+        pizzaStoreNameTxt.text = data.name
+
+        Glide.with(mContext).load(data.imgUrl).into(pizzaStoreImg)
 
         return row
 
